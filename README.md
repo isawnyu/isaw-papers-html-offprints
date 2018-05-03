ec77ad2731c93ec30a29fad381863c73bcd7f444
Fanny Mezard <fanny@NACOLA01.ISAW.NYU.EDU>
2018-05-03 17:08:40 -0400

    First attempt at creating standalone files

---
+++ README.md
@@ -0,0 +1,5 @@
+ISAW PAPERS 
+-----------------------
+ISAW Papers is a online journal of article length scholarship. 
+
+This repository presents a version of the articles that can be downloaded as standalone XHTML file
---
+++ standalone_xhtml.py
@@ -0,0 +1,48 @@
+import base64
+from bs4 import BeautifulSoup    
+import os
+import re
+
+i64 = []
+
+
+for j in range(1, 14):
+
+	# soup
+	with open("isaw-papers-awdl/"+str(j)+"/index.xhtml", "r") as article :
+		soup = BeautifulSoup(article, "lxml")
+	images = soup.find_all("img", {"src" : re.compile("images/*")}) 
+
+	# Replacing images by base64 images for the articles that are illustrated 
+	if os.path.isdir(str(j)+"/images_small/images") :
+		for filename in os.listdir(str(j)+"/images_small/images"):
+		    with open(str(j)+"/images_small/images/"+filename, "rb") as imageFile:
+		        im64 = base64.b64encode(imageFile.read())
+		        i64.append(str(im64).replace("b'", "").replace("'",""))
+
+		for i in range(0, len(images)):
+			source = images[i]["src"]
+			source = source.replace("images/", "").replace(".png", "").replace(".jpg", "")
+			images[i].wrap(soup.new_tag("a", href="http://dlib.nyu.edu/awdl/isaw/isaw-papers/"+str(j)+"/#"+source))
+			images[i]["src"] = "data:image/png;base64,"+str(i64[i])
+
+	# putting the css in the xhtml file
+	if os.path.exists("isaw-papers-awdl/"+str(j)+"/isaw-papers.css"):
+		with open("isaw-papers-awdl/"+str(j)+"/isaw-papers.css", "r") as css_file :
+			css = css_file.read()
+	elif os.path.exists("isaw-papers-awdl/"+str(j)+"/isaw-publications.css"):
+		with open("isaw-papers-awdl/"+str(j)+"/isaw-publications.css", "r") as css_file :
+			css = css_file.read()
+
+	soup.head.append(soup.new_tag("style"))
+	soup.head.style.append(css)
+
+	# creating the standlone xhtml file
+	with open(str(j)+"/standlone"+str(j)+".xhtml", "w") as article :
+		article.write(str(soup))
+
+	# adding the link to the download file
+	with open("index.md", "a") as download_page:
+		download_page.write("<a href='"+str(j)+"/standlone-"+str(j)+".xhtml' download>Click to Download Paper " + str(j) + "</a>\n<a href='"+str(j)+"/standlone-"+str(j)+".xhtml'>Click to see in browser Paper "+str(j) + "</a>\n\n")
+
+
